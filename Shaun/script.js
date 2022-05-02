57e5466c8e6b3105fb3d893ea9b021412873f9eb
Shaun <n10537848@qut.edu.au>
2022-05-02 18:54:41 +1000

    init

---
+++ Shaun/script.js
@@ -0,0 +1,3 @@
+const img1 = (document.getElementById("myImg").src = pic1.jpg);
+const img2 = (document.getElementById("myImg").src = pic2.jpg);
+const img3 = (document.getElementById("myImg").src = pic3.jpg);
