eab7b120d73701ed929fe1c58505685111db5a10
Shaun <n10537848@qut.edu.au>
2022-05-05 12:04:19 +1000

    experimenting changing sky

--- Shaun/script.js
+++ Shaun/script.js
@@ -1,3 +1,15 @@
-const img1 = (document.getElementById("myImg").src = pic1.jpg);
-const img2 = (document.getElementById("myImg").src = pic2.jpg);
-const img3 = (document.getElementById("myImg").src = pic3.jpg);
+const sceneEl = document.querySelector("a-scene");
+const skyEl = sceneEl.querySelector("#background");
+console.log(skyEl);
+
+AFRAME.registerComponent("button-interaction-handler", {
+  init: function () {
+    this.el.addEventListener("mouseenter", function () {
+      skyEl.setAttribute("src", "#chineseTemple");
+    });
+
+    this.el.addEventListener("mouseleave", function () {
+      skyEl.setAttribute("src", "#backgroundInsideHouse");
+    });
+  },
+});
